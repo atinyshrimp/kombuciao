b6f2a10f400c644db13a1597d55ea88d47184407
Joyce <99140907+atinyshrimp@users.noreply.github.com>
2025-06-29 21:09:39 +0200

    feat(constants): add various flavor options

---
+++ frontend/constants/index.ts
@@ -0,0 +1,8 @@
+export const FLAVORS = {
+	citron: "🍋 Citron",
+	peche: "🍑 Pêche",
+	fruits_rouges: "🍒 Fruits Rouges",
+	dragon: "🐉 Dragon",
+	menthe: "🍃 Menthe",
+	gingembre_hibiscus: "🌺 Gingembre Hibiscus",
+};
