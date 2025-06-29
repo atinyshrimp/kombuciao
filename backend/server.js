6ff1f509b128d81cfc9fb4bc1ee10c52b011bb87
Joyce <99140907+atinyshrimp@users.noreply.github.com>
2025-06-29 02:30:59 +0200

    feat(backend): add store routes to server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const connectDB = require("./config/db.js");
 const { PORT } = require("./config");
 
+const storeRoutes = require("./routes/store.js");
+
 connectDB();
 
 const app = express();
@@ -10,6 +12,7 @@ app.use(cors());
 app.use(express.json({ limit: "2mb" })); // body-parser
 
 app.get("/", (_, res) => res.json({ message: "Ciao Kombucha Locator API 😎" }));
+app.use("/stores", storeRoutes);
 
 /* Global error handler */
 app.use((err, _, res, __) => {
