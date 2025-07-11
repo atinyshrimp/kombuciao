2fdbd65c64c1797e91005672aec67a8a0360ff0b
Joyce <99140907+atinyshrimp@users.noreply.github.com>
2025-07-11 20:02:39 +0200

    fix(map): update type assertion

--- frontend/components/map.tsx
+++ frontend/components/map.tsx
@@ -8,7 +8,7 @@ import { Store } from "@/types/store";
 import StatsCard from "./cards/StatsCard";
 
 // Fix for default markers
-delete (L.Icon.Default.prototype as any)._getIconUrl;
+delete (L.Icon.Default.prototype as { _getIconUrl?: () => string })._getIconUrl;
 L.Icon.Default.mergeOptions({
 	iconRetinaUrl:
 		"https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
