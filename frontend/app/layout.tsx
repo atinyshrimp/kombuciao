eddced5b78d82bbe55ea62c1dc57a5a5e01e7fe0
Joyce <99140907+atinyshrimp@users.noreply.github.com>
2025-07-14 16:36:05 +0200

    fix(layout): integrate Toaster component

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -76,6 +77,7 @@ export default function RootLayout({
 						</a>
 					</footer>
 				</div>
+				<Toaster richColors position="top-center" />
 			</body>
 		</html>
 	);
